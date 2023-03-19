e3337d8fce1086d04e685024ce025ef028c2c093
Cinquin Andy <andy.cinquin@infotel.com>
2023-03-19 16:57:32 +0100

    code review

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+let var1 = 5;
+let var2 = 10;
+let var3 = var1 + var2;
+console.log(var3);
+
+// get and log title id balise
+let title = document.getElementById("title");
+console.log(title);
+// change content of title
+title.innerHTML = "Hello World !";
+
+// change color of title to red
+title.style.color = "red";
+
