fcec710fdcd44c6e00614bcbb7f65d6c9e55d275
Mathéo Yaké <matheoyake21@hotmail.com>
2023-03-24 08:49:16 +0100

    slidng cards avant ajout du JS pour animer

--- index.js
+++ index.js
@@ -9,17 +9,3 @@ window.addEventListener(
     false
 );
 
-
-    var swiper = new Swiper(".slide-content", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-},
-    navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-},
-});
