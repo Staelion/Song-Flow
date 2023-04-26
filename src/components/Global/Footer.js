68285612b40e72ada7d37dea99f9ea9c8f0b1ee3
Mathéo Yaké <matheoyake21@hotmail.com>
2023-04-26 14:09:03 +0200

    a

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -3,84 +3,95 @@ import Link from 'next/link'
 
 function Footer(props) {
 	return (
-		<>
-			<div className={'mt-60 border-t-8 border-mainorange-500 pt-20'}>
-				<div className="m-auto max-w-screen-2xl">
-					<div className={'flex max-w-full justify-around font-oswald'}>
-						<div className={'flex flex-col justify-center'}>
-							<h4 className="mb-4  p-3.5 text-4xl font-semibold text-white ">
-								L{"'"}entreprise
-							</h4>
-							<div className="flex justify-center">
-								<Link
-									href={'/qui_sommes_nous'}
-									className={
-										'pt-3 text-xl font-semibold text-white hover:text-mainaccent-700'
-									}
-								>
-									A propos de nous
-								</Link>
-							</div>
-
-							<div className="flex justify-center">
-								<Link
-									href={'/nos_services'}
-									className={
-										'pt-3 text-xl font-semibold text-white hover:text-mainaccent-700'
-									}
-								>
-									Nos services
-								</Link>
-							</div>
-
-							<div className="flex justify-center">
-								<Link
-									href={'/politique_de_confidentialite'}
-									className={
-										'pb-5 pt-3 text-xl font-semibold text-white hover:text-mainaccent-700'
-									}
-								>
-									Politique de confidentialité
-								</Link>
-							</div>
+		<div className={'mt-60 border-t-8 border-mainorange-500 pt-20'}>
+			<div className="m-auto max-w-screen-2xl">
+				<div className={'flex max-w-full justify-around font-oswald'}>
+					<div className={'flex flex-col justify-center'}>
+						<h4 className="mb-4  p-3.5 text-4xl font-semibold text-white ">
+							L{"'"}entreprise
+						</h4>
+						<div className="flex justify-center">
+							<Link
+								href={'/qui_sommes_nous'}
+								className={
+									'pt-3 text-xl font-semibold text-white hover:text-mainaccent-700'
+								}
+							>
+								A propos de nous
+							</Link>
 						</div>
 
-						<div>
-							<h4 className={'mb-4 p-3.5 text-4xl font-semibold text-white '}>
-								Obtenir de l{"'"}aide
-							</h4>
-
-							<div className="flex justify-center">
-								<Link
-									href={'/faq'}
-									className={
-										'pt-3 text-xl font-semibold text-white hover:text-mainaccent-700'
-									}
-								>
-									FAQ
-								</Link>
-							</div>
-
-							<div className="flex justify-center">
-								<Link
-									href={'/faq'}
-									className={
-										'pt-3 text-xl font-semibold text-white hover:text-mainaccent-700'
-									}
-								>
-									Options de paiment
-								</Link>
-							</div>
+						<div className="flex justify-center">
+							<Link
+								href={'/nos_services'}
+								className={
+									'pt-3 text-xl font-semibold text-white hover:text-mainaccent-700'
+								}
+							>
+								Nos services
+							</Link>
 						</div>
-						<div>
-							<h4 className={'mb-4 p-3.5 text-4xl font-semibold text-white '}>
-								Suivez-nous
-							</h4>
+
+						<div className="flex justify-center">
+							<Link
+								href={'/politique_de_confidentialite'}
+								className={
+									'pb-5 pt-3 text-xl font-semibold text-white hover:text-mainaccent-700'
+								}
+							>
+								Politique de confidentialité
+							</Link>
+						</div>
+					</div>
+
+					<div>
+						<h4 className={'mb-4 p-3.5 text-4xl font-semibold text-white '}>
+							Obtenir de l{"'"}aide
+						</h4>
+
+						<div className="flex justify-center">
+							<Link
+								href={'/faq'}
+								className={
+									'pt-3 text-xl font-semibold text-white hover:text-mainaccent-700'
+								}
+							>
+								FAQ
+							</Link>
+						</div>
+
+						<div className="flex justify-center">
+							<Link
+								href={'/faq'}
+								className={
+									'pt-3 text-xl font-semibold text-white hover:text-mainaccent-700'
+								}
+							>
+								Options de paiment
+							</Link>
+						</div>
+					</div>
+
+					<div>
+						<h4 className={'mb-4 p-3.5 text-4xl font-semibold text-white '}>
+							Suivez-nous
+						</h4>
+
+						<div className="flex justify-center">
+							<Link
+								href={'https://www.instagram.com/Song_Flow'}
+								target={'_blank'}
+								className={'flex justify-center rounded-full bg-white'}
+							>
+								<svg className={'flex h-1/5 w-1/5 justify-center bg-black'}>
+									src={'/instagram'}
+								</svg>
+							</Link>
 						</div>
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	)
 }
 
