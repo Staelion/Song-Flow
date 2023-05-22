b70983a989ea55fcd193761ccb81608ba2326e9d
Mathéo Yaké <matheoyake21@hotmail.com>
2023-05-22 21:37:11 +0200

    test flashcard

---
+++ src/components/Global/Flashcard.js
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+
+
+
+
+const flashcard = [
+
+{
+ name: '',
+ description: '',
+ ],
+},
+
+{
+ name: '',
+ description: '',
+ ],
+},
+
+{
+ name: '',
+ description: '',
+ ],
+},
+
+{
+ name: '',
+ description: '',
+ ],
+},
+
+{
+ name: '',
+ description: '',
+ ],
+},
+]
+
+
+export default function Flashcard () {
+    return (
+    <>
+        <div className="isolate overflow-hidden ">
+        <div className=" "
+
+    )
+}
