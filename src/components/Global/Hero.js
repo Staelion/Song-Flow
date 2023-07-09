e5ed2a95df8b6112400ee83c7aec7d630364ff50
Mathéo Yaké <matheoyake21@hotmail.com>
2023-07-09 16:25:40 +0200

    Auto stash before merge of "Login_page" and "origin/Login_page"

--- src/components/Global/Hero.js
+++ src/components/Global/Hero.js
@@ -17,6 +17,7 @@ function Hero(props) {
 					</p>
 
 					<div className={'flex gap-4'}>
+<<<<<<< Updated upstream
          
 						<Link href="/login">
 							<button
@@ -26,6 +27,15 @@ function Hero(props) {
 							>
 								Je me lance
 							</button>
+=======
+						<Link
+							href={'/login'}
+							className={
+								'shadow-inner-[0_-4px_5px_#7c20ef] flex items-center rounded-2xl border-2 border-mainaccent-700/0 bg-mainaccent-700/100 px-12 py-4 text-2xl text-white shadow-[0_4px_5px_#31066f] transition delay-100 ease-in-out hover:border-mainaccent-700 hover:bg-mainaccent-700/0 hover:text-mainaccent-700  hover:shadow-none'
+							}
+						>
+							Je me lance
+>>>>>>> Stashed changes
 						</Link>
 
 						<button
