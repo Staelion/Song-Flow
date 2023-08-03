2d923a087e2b380f25d129dd0ba841c61b971c05
Mathéo Yaké <matheoyake21@hotmail.com>
2023-08-03 10:42:01 +0200

    suite profil modif

--- src/components/Global/Profil_edit.component.js
+++ src/components/Global/Profil_edit.component.js
@@ -102,13 +102,10 @@ function ProfilEditComponent(props) {
 				<div className="">
 					<div className="m-6 flex h-full flex-col">
 						<form>
-							<div className="border-b border-gray-900/10 pb-12">
-								<h2 className="text-base font-semibold leading-7 text-gray-900">
-									Personal Information
+							<div className=" pb-12">
+								<h2 className="font-sans text-4xl font-semibold leading-7 text-black">
+									Informations personnelles
 								</h2>
-								<p className="mt-1 text-sm leading-6 text-gray-600">
-									Use a permanent address where you can receive mail.
-								</p>
 
 								<div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 									<div className="sm:col-span-3">
@@ -152,7 +149,7 @@ function ProfilEditComponent(props) {
 											htmlFor="email"
 											className="block text-sm font-medium leading-6 text-gray-900"
 										>
-											Email address
+											Adresse mail
 										</label>
 										<div className="mt-2">
 											<input
@@ -176,7 +173,7 @@ function ProfilEditComponent(props) {
 											<select
 												id="country"
 												name="country"
-												autoComplete="country-name"
+												autoComplete="Nom du pays"
 												className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
 											>
 												<option value="France" selected="selected">
@@ -481,80 +478,206 @@ function ProfilEditComponent(props) {
 										</div>
 									</div>
 
-									<div className="col-span-full">
-										<label
-											htmlFor="street-address"
-											className="block text-sm font-medium leading-6 text-gray-900"
-										>
-											Street address
-										</label>
-										<div className="mt-2">
-											<input
-												type="text"
-												name="street-address"
-												id="street-address"
-												autoComplete="street-address"
-												className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-											/>
-										</div>
-									</div>
-
 									<div className="sm:col-span-2 sm:col-start-1">
 										<label
-											htmlFor="city"
+											htmlFor="day"
 											className="block text-sm font-medium leading-6 text-gray-900"
 										>
-											City
+											day
 										</label>
 										<div className="mt-2">
-											<input
-												type="text"
-												name="city"
-												id="city"
-												autoComplete="address-level2"
+											<select
+												name="day"
+												id="day"
+												autoComplete="day"
 												className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-											/>
+											>
+												<option value={'day'} selected={'selected'}>
+													Jour
+												</option>
+												<option value="01">01</option>
+												<option value="02">02</option>
+												<option value="03">03</option>
+												<option value="04">04</option>
+												<option value="05">05</option>
+												<option value="06">06</option>
+												<option value="07">07</option>
+												<option value="08">08</option>
+												<option value="09">09</option>
+												<option value="10">10</option>
+												<option value="11">11</option>
+												<option value="12">12</option>
+												<option value="13">13</option>
+												<option value="14">14</option>
+												<option value="15">15</option>
+												<option value="16">16</option>
+												<option value="17">17</option>
+												<option value="18">18</option>
+												<option value="19">19</option>
+												<option value="20">20</option>
+												<option value="21">21</option>
+												<option value="22">22</option>
+												<option value="23">23</option>
+												<option value="24">24</option>
+												<option value="25">25</option>
+												<option value="26">26</option>
+												<option value="27">27</option>
+												<option value="28">28</option>
+												<option value="29">29</option>
+												<option value="30">30</option>
+												<option value="31">31</option>
+											</select>
 										</div>
 									</div>
 
 									<div className="sm:col-span-2">
 										<label
-											htmlFor="region"
+											htmlFor="month"
 											className="block text-sm font-medium leading-6 text-gray-900"
 										>
-											State / Province
+											Month
 										</label>
 										<div className="mt-2">
-											<input
-												type="text"
-												name="region"
-												id="region"
+											<select
+												name="month"
+												id="month"
 												autoComplete="address-level1"
 												className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-											/>
+											>
+												<option value={'mois'} selected={'selected'}>
+													Mois
+												</option>
+												<option value="01">Janvier</option>
+												<option value="02">Fevrier</option>
+												<option value="03">Mars</option>
+												<option value="04">Avril</option>
+												<option value="05">Mai</option>
+												<option value="06">Juin</option>
+												<option value="07">Juillet</option>
+												<option value="08">Août</option>
+												<option value="09">Septenmbre</option>
+												<option value="10">Octobre</option>
+												<option value="11">Novembre</option>
+												<option value="12">Décembre</option>
+											</select>
 										</div>
 									</div>
 
 									<div className="sm:col-span-2">
 										<label
-											htmlFor="postal-code"
+											htmlFor="year"
 											className="block text-sm font-medium leading-6 text-gray-900"
 										>
-											ZIP / Postal code
+											Year
 										</label>
 										<div className="mt-2">
-											<input
-												type="text"
-												name="postal-code"
-												id="postal-code"
-												autoComplete="postal-code"
+											<select
+												name="year"
+												id="year"
+												autoComplete="year"
 												className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-											/>
+											>
+												<option value="année" selected="selected">
+													Année
+												</option>
+
+												<option value="1940">1940</option>
+												<option value="1941">1941</option>
+												<option value="1942">1942</option>
+												<option value="1943">1943</option>
+												<option value="1944">1944</option>
+												<option value="1945">1945</option>
+												<option value="1946">1946</option>
+												<option value="1947">1947</option>
+												<option value="1948">1948</option>
+												<option value="1949">1949</option>
+												<option value="1950">1950</option>
+												<option value="1951">1951</option>
+												<option value="1952">1952</option>
+												<option value="1953">1953</option>
+												<option value="1954">1954</option>
+												<option value="1955">1955</option>
+												<option value="1956">1956</option>
+												<option value="1957">1957</option>
+												<option value="1958">1958</option>
+												<option value="1959">1959</option>
+												<option value="1960">1960</option>
+												<option value="1961">1961</option>
+												<option value="1962">1962</option>
+												<option value="1963">1963</option>
+												<option value="1964">1964</option>
+												<option value="1965">1965</option>
+												<option value="1966">1966</option>
+												<option value="1967">1967</option>
+												<option value="1968">1968</option>
+												<option value="1969">1969</option>
+												<option value="1970">1970</option>
+												<option value="1971">1971</option>
+												<option value="1972">1972</option>
+												<option value="1973">1973</option>
+												<option value="1974">1974</option>
+												<option value="1975">1975</option>
+												<option value="1976">1976</option>
+												<option value="1977">1977</option>
+												<option value="1978">1978</option>
+												<option value="1979">1979</option>
+												<option value="1980">1980</option>
+												<option value="1981">1981</option>
+												<option value="1982">1982</option>
+												<option value="1983">1983</option>
+												<option value="1984">1984</option>
+												<option value="1985">1985</option>
+												<option value="1986">1986</option>
+												<option value="1987">1987</option>
+												<option value="1988">1988</option>
+												<option value="1989">1989</option>
+												<option value="1990">1990</option>
+												<option value="1991">1991</option>
+												<option value="1992">1992</option>
+												<option value="1993">1993</option>
+												<option value="1994">1994</option>
+												<option value="1995">1995</option>
+												<option value="1996">1996</option>
+												<option value="1997">1997</option>
+												<option value="1998">1998</option>
+												<option value="1999">1999</option>
+												<option value="2000">2000</option>
+												<option value="2001">2001</option>
+												<option value="2002">2002</option>
+												<option value="2003">2003</option>
+												<option value="2004">2004</option>
+												<option value="2005">2005</option>
+												<option value="2006">2006</option>
+												<option value="2007">2007</option>
+												<option value="2008">2008</option>
+												<option value="2009">2009</option>
+												<option value="2010">2010</option>
+												<option value="2011">2011</option>
+												<option value="2012">2012</option>
+												<option value="2013">2013</option>
+												<option value="2014">2014</option>
+												<option value="2015">2015</option>
+												<option value="2016">2016</option>
+												<option value="2017">2017</option>
+												<option value="2018">2018</option>
+												<option value="2019">2019</option>
+												<option value="2020">2020</option>
+												<option value="2021">2021</option>
+												<option value="2022">2022</option>
+												<option value="2023">2023</option>
+											</select>
 										</div>
 									</div>
 								</div>
 							</div>
 						</form>
+						<button
+							className={
+								' ml-14 flex w-2/5 items-center justify-center rounded-2xl bg-mainaccent-700/100 px-12 py-4 text-xl text-white shadow-[0_4px_5px_#31066f] transition delay-100 ease-in-out hover:brightness-125'
+							}
+						>
+							Enregistrer
+						</button>
 					</div>
 				</div>
 			</main>
