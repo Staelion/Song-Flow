c23f847705e1bff42a9cdc368db378d8fa5e0a20
Mathéo Yaké <matheoyake21@hotmail.com>
2023-10-22 16:50:23 +0200

    template question oui non ok

--- src/components/Global/Steps/Step02.component.js
+++ src/components/Global/Steps/Step02.component.js
@@ -6,7 +6,9 @@ function Step02Component() {
 	const { steps, stepsCount, nextStep, previousStep, clickOnStep } =
 		useContext(StepsContext)
 
-	const [enabled, setEnabled] = useState(true)
+	const [toggleState1, setToggleState1] = useState(false)
+	// une ligne par toggle différent
+	// const [toggleState2, setToggleState2] = useState(false)
 
 	return (
 		<>
@@ -14,24 +16,48 @@ function Step02Component() {
 			<br />
 			<br />
 			<br />
-			{/*template question oui / non*/}
-
-			<div className={'flex '}>
-				<Switch
-					checked={enabled}
-					onChange={setEnabled}
-					className={`${
-						enabled ? 'bg-mainaccent-700' : 'bg-amber-950'
-					} relative inline-flex h-8 w-16 items-center rounded-full`}
+			template question oui / non
+			<div className={'flex h-[45px] '}>
+				{/*Numéro de la question*/}
+				<div
+					className={
+						' flex h-10 w-10 items-center justify-center rounded-full bg-mainaccent-700 text-white'
+					}
 				>
-					<span className="sr-only">Enable notifications</span>
-					<span
+					01
+				</div>
+				{/*titre de la question */}
+				<div
+					className={
+						'flex w-1/3 max-w-full items-center justify-center  pl-5 font-oswald text-white'
+					}
+				>
+					Utilisation de samples ?
+				</div>
+				{/*switch oui non */}
+				<div className={' flex items-center justify-center px-5'}>
+					<Switch
+						checked={toggleState1}
+						onChange={setToggleState1}
 						className={`${
-							enabled ? 'translate-x-10' : 'translate-x-1'
-						} inline-block h-4 w-4 transform rounded-full bg-white transition`}
-					/>
-				</Switch>
+							toggleState1 ? 'bg-mainaccent-700' : 'bg-amber-950'
+						} relative inline-flex h-5 w-10 items-center rounded-full`}
+					>
+						<span className="sr-only">Enable this option</span>
+						<span
+							className={`${
+								toggleState1 ? 'translate-x-5' : 'translate-x-1'
+							} inline-block h-4 w-4 transform rounded-full bg-white transition`}
+						/>
+					</Switch>
+				</div>
 			</div>
+			<br />
+			template question libre
+			<br />
+			template question a slider / slider double
+			<br />
+			template question a choix multiples (combobox Headless UI)
 		</>
 	)
 }
