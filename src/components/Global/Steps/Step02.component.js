d60e483d9e18e2b150d076b345702005742cce4b
Mathéo Yaké <matheoyake21@hotmail.com>
2023-10-24 18:57:06 +0200

    idée de template pour question <input>
    cf video yt

--- src/components/Global/Steps/Step02.component.js
+++ src/components/Global/Steps/Step02.component.js
@@ -10,6 +10,24 @@ function Step02Component() {
 	// une ligne par toggle différent
 	// const [toggleState2, setToggleState2] = useState(false)
 
+	const [serviceList, setServiceList] = useState([{ service: '' }])
+
+	const handleServiceChange = (e, index) => {
+		const { name, value } = e.target
+		const list = [...serviceList]
+		list[index][name] = value
+		setServiceList(list)
+	}
+
+	const handleServiceRemove = index => {
+		const list = [...serviceList]
+		list.splice(index, 1)
+		setServiceList(list)
+	}
+
+	const handleServiceAdd = () => {
+		setServiceList([...serviceList, { service: '' }])
+	}
 	return (
 		<>
 			<div>step 02</div>
@@ -54,6 +72,55 @@ function Step02Component() {
 			</div>
 			<br />
 			template question libre
+			<form className="App" autoComplete="off">
+				<div className="form-field">
+					<label htmlFor="service">Service(s)</label>
+					{serviceList.map((singleService, index) => (
+						<div key={index} className="services">
+							<div className="first-division">
+								<input
+									name="service"
+									type="text"
+									id="service"
+									value={singleService.service}
+									onChange={e => handleServiceChange(e, index)}
+									required
+								/>
+								{serviceList.length - 1 === index && serviceList.length < 5 && (
+									<button
+										type="button"
+										onClick={handleServiceAdd}
+										className="add-btn"
+									>
+										<span>Add a Service</span>
+									</button>
+								)}
+							</div>
+							<div className="second-division">
+								{serviceList.length !== 1 && (
+									<button
+										type="button"
+										onClick={() => handleServiceRemove(index)}
+										className="remove-btn"
+									>
+										<span>Remove</span>
+									</button>
+								)}
+							</div>
+						</div>
+					))}
+				</div>
+				<div className="output">
+					<h2>Output</h2>
+					{serviceList &&
+						serviceList.map((singleService, index) => (
+							<ul key={index}>
+								{singleService.service && <li>{singleService.service}</li>}
+							</ul>
+						))}
+				</div>
+			</form>
+			)
 			<br />
 			template question a slider / slider double
 			<br />
