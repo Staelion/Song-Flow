57e7a758dc0485ae95b8b869f44731e8ea37619d
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-01 17:14:23 +0100

    🔧 Update Logoterre component styling
    The Logoterre component's CSS class has been modified to fix the z-index value, improving the positioning of the image. The previous value was -10 and has been changed to 0.

--- src/components/Global/Landing page/Logoterre.component.js
+++ src/components/Global/Landing page/Logoterre.component.js
@@ -4,14 +4,14 @@ import Image from 'next/image'
 function LogoterreComponent(props) {
 	return (
 		<>
-			<div className="bottom-30 fixed -right-72 -z-10 flex h-[800px] w-[800px] ">
+			<div className="bottom-30 fixed -right-72 z-0 flex h-[800px] w-[800px] ">
 				<Image
 					className={'logoterre'}
 					src={'/images/planete-terre.svg'}
 					alt={'Logo planète terre'}
 					width={'600'}
 					height={'600'}
-				></Image>
+				/>
 			</div>
 		</>
 	)
